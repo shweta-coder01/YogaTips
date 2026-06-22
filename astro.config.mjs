3a00bcbc87f303308bc31259cea4b56f838b8498
shweta-coder01 <shweta-coder01@users.noreply.github.io>
2026-06-22 14:29:00 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shweta-coder01.github.io',
   base: '/Global-Yoga-Tips',
   vite: {
     plugins: [tailwindcss()]
